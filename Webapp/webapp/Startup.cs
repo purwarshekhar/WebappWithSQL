0251c02df4c5b1bd8083a0b4438c504a01a68b2a
purwarshekhar <purwar.shekhar@gmail.com>
2021-06-04 10:39:12 -0400

    Update Startup.cs

--- Webapp/webapp/Startup.cs
+++ Webapp/webapp/Startup.cs
@@ -40,7 +40,7 @@ namespace az203_core
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS va lue is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
